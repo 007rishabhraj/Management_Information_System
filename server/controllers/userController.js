993bcd09c4b849367f79e4b8c77c30629243d2a5
aryan210801 <110852048+aryan210801@users.noreply.github.com>
2024-08-07 14:21:38 +0530

    created a login functino

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,44 @@ import bcrypt from 'bcrypt';
 import JWT from 'jsonwebtoken';
 import sendToken from "../utils/sendToken.js";
 
+const login = async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  try {
+    // Find user by email
+    const user = await User.findOne({ email });
+    console.log("hm hi hai user");
+    console.log(user);
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({
+        message: "User does not exist! Please register",
+      });
+    }
+
+    // Validate password
+    console.log("ye kya ho gya hai");
+    console.log(typeof password);
+    console.log(typeof user.password);
+    const isValid = await bcrypt.compareSync(password, user.password);
+    if (!isValid) {
+      return res.status(400).json({
+        status: "Error",
+        message: "Password does not match",
+      });
+    }
+
+    // Generate JWT token
+    const token = JWT.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    sendToken(user, 200, token, res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error"); // Handle server error
+  }
+};
+
 
 const createUser = async(req, res) => {
 
