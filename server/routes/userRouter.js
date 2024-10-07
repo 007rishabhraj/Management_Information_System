26e43421c458bc970985c38ca60be8c6fa52ac5d
aryan210801 <110852048+aryan210801@users.noreply.github.com>
2024-10-07 23:36:41 +0530

    complain backend added

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
 import userController from "../controllers/userController.js";
 import verifyUser from "../middleware/verifyUser.js";
+import complainController from "../controllers/complainController.js";
 
 export const userRouter = express.Router();
 
 userRouter.route("/").all(verifyUser).get(userController.getUser);
 userRouter.route("/signup").post(userController.createUser);
 userRouter.route("/login").post(userController.login);
+
+userRouter.route("/complain").post(complainController.createComplain);
+userRouter.route("/complain").get(complainController.getComplain);
+userRouter.route("/complain").delete(complainController.deleteComplain);
