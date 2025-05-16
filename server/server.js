5e735f61ca7f499e8f08ba19d31fb133a2c2d005
Rishabh Raj <111532754+007rishabhraj@users.noreply.github.com>
2025-05-16 17:20:19 +0530

    added / get in  server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,9 @@ const server = app.listen(port, () => {
   console.log("Server running on ", port);
 });
 
-
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 // handling unhandled rejections
 process.on("unhandledRejection", (err) => {
